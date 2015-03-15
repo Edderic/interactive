15bc4b19140b28714f3fc69f5e57134be29e5a68
Edderic Ugaddan <edderic@gmail.com>
2015-03-15 11:55:21 -0400

    [#90360700] refactor. extract private methods

--- lib/interactive/response.rb
+++ lib/interactive/response.rb
@@ -1,27 +1,48 @@
 module Interactive
   class Response
     def initialize(*args)
-      if args.first.class.ancestors.include?(Enumerable)
-        @args = args.first
-      else
-        @args = args
-      end
+      set_args(args)
 
-      raise ArgumentError, "wrong number of arguments (need at least two arguments)" if @args.length == 1
-      raise ArgumentError, "may not use :invalid or 'invalid' as an argument. May not overwrite Interactive::Response#invalid" if @args.map(&:to_s).include?('invalid')
-
-      raise ArgumentError, "may not have keyword options that have the same first letter." if @args.map{|arg| arg[0]}.uniq.length != @args.length
+      check_validity(args)
 
       @_response = STDIN.gets.chomp
 
+      define_methods
+      define_invalid
+    end
+
+    private
+
+    def check_validity(args)
+      raise ArgumentError, "wrong number of arguments (need at least two arguments)" if @args.length == 1
+      raise ArgumentError, "may not use :invalid or 'invalid' as an argument. May not overwrite Interactive::Response#invalid" if @args.map(&:to_s).include?('invalid')
+
+      raise ArgumentError, "may not have keyword options that have the same first letter." if first_chars_unique
+    end
+
+    def define_methods
       @args.each do |arg|
         define_singleton_method "#{arg}?" do
           @_response.match(/^#{arg[0]}$/i) ? true : false
         end
       end
+    end
 
+    def define_invalid
       define_singleton_method "invalid?" do
         methods(false).reject {|m| m == :invalid? }.none? {|m| send(m) }
+      end
+    end
+
+    def first_chars_unique
+      @args.map{|arg| arg[0]}.uniq.length != @args.length
+    end
+
+    def set_args(args)
+      if args.first.class.ancestors.include?(Enumerable)
+        @args = args.first
+      else
+        @args = args
       end
     end
   end
